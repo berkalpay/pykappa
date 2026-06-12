b1abb530375a0ff2e20d5e959aa490f1d9e606a5
berkalpay <46028827+berkalpay@users.noreply.github.com>
2026-06-12 11:01:30 -0400

    docs: switch Jupyter kernel from TCP to IPC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,10 @@ autodoc_member_order = "bysource"
 autoclass_content = "class"
 napoleon_google_docstring = True
 nbsphinx_execute = "always"
-nbsphinx_execute_arguments = ["--InlineBackend.figure_formats={'svg'}"]
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg'}",
+    "--KernelManager.transport='ipc'",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
