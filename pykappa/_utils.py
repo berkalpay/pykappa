777e27c485e87c54a9c1cb5314dc9cd9061448b3
berkalpay <46028827+berkalpay@users.noreply.github.com>
2026-06-18 11:26:13 -0400

    remove unused OrderedSet class

--- pykappa/_utils.py
+++ pykappa/_utils.py
@@ -42,23 +42,6 @@ def rejection_sample(population: Iterable, excluded: Iterable, max_attempts: int
     return random.choice(valid_choices)
 
 
-class OrderedSet[T]:
-    def __init__(self, items: Optional[Iterable[T]] = None):
-        self.dict = dict() if items is None else dict.fromkeys(items)
-
-    def __iter__(self):
-        yield from self.dict
-
-    def __len__(self):
-        return len(self.dict)
-
-    def add(self, item: Any) -> None:
-        self.dict[item] = None
-
-    def remove(self, item: Any) -> None:
-        del self.dict[item]
-
-
 class Counted:
     """Assigns a unique integer ID to each instance, starting from 0."""
 
