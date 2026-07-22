7a4a52c31a5d9e6340efb3799990898a645ff866
berkalpay <46028827+berkalpay@users.noreply.github.com>
2026-07-22 11:52:01 -0400

    remove Monitor from __init__.py

--- pykappa/__init__.py
+++ pykappa/__init__.py
@@ -2,15 +2,5 @@ from .system import System
 from .mixture import Mixture
 from .rule import Rule
 from .pattern import Pattern, Component, Agent, Site
-from .analysis import Monitor
 
-__all__ = [
-    "System",
-    "Monitor",
-    "Mixture",
-    "Rule",
-    "Pattern",
-    "Component",
-    "Agent",
-    "Site",
-]
+__all__ = ["System", "Mixture", "Rule", "Pattern", "Component", "Agent", "Site"]
