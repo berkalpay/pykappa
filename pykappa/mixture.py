d9971f0a4e03db6619ea706741e426c4bf18333e
berkalpay <46028827+berkalpay@users.noreply.github.com>
2026-08-07 17:18:57 -0400

    add Mixture.kappa_str_with_agent_ids

--- pykappa/mixture.py
+++ pykappa/mixture.py
@@ -95,6 +95,13 @@ class Mixture:
         )
 
     @property
+    def kappa_str_with_agent_ids(self) -> str:
+        """Kappa representation with IDs, with one component per line."""
+        return "\n".join(
+            component.kappa_str_with_agent_ids for component in self
+        )
+
+    @property
     def component_tracking(self) -> bool:
         """Whether connected components are being tracked."""
         return self._components is not None
